3f00d82ecd87d5ebe0ab6759a91f0cf6135078df
Perry <yipurui@gmail.com>
2022-08-01 17:35:32 -0800

    feature: show pendingEvent

---
+++ app/src/main/java/com/example/sport_events_scheduler/PendingEvent.java
@@ -0,0 +1,40 @@
+package com.example.sport_events_scheduler;
+
+import com.google.firebase.database.Exclude;
+
+public class PendingEvent {
+
+//    @Exclude
+//    private String key;
+    private int capacity, joined;
+    private String end, id, location, name, start;
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getJoined() {
+        return joined;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
